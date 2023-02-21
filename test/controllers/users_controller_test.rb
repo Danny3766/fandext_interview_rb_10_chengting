d86e39c908f4fecddc51eb09efe7c69573c23708
Ting <woodming9889@gmail.com>
2023-02-21 10:50:18 +0800

    implement test of create user succeeded & create user failed

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,8 +11,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "create user succeeded" do
+    post users_path, params: { user: { email: "test@example.com", first_name: "Danny", last_name: "Hsu", address: "123 Main St" } }
+    assert_redirected_to users_path
+    assert_equal "Welcome to fandnext", flash[:success]
   end
 
   test "create user failed" do
+    post users_path, params: { user: { email: "test@example.invalid", first_name: "Danny", last_name: "Hsu", address: "123 Main St" } }
+    assert_template :new
+    assert_equal "Domain invalid", flash[:warning]
   end
 end
